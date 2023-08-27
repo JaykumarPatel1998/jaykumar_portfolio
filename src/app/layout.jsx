e3172b6182d2f01c060a6f3920a0675d3e93d3b5
jaykumar <slapbot6@gmail.com>
2023-08-27 01:05:20 -0400

    added several animated svgs to the website

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,8 +8,6 @@ const RootLayout = ({ children }) => {
 
   useEffect(() => {
 
-    // Particle follows the mouse
-
     const canvas = canvasRef.current
     const c = canvas.getContext('2d')
 
@@ -50,21 +48,9 @@ const RootLayout = ({ children }) => {
         this.velocity = 0.05
       }
 
-      // update() {
-      //   const lastPoint = { x: this.x, y: this.y }
-      //   this.radians += this.velocity;
-
-      //   //drag effect
-      //   this.lastMouse.x += (mouse.x - this.lastMouse.x) * 0.05
-      //   this.lastMouse.y += (mouse.y - this.lastMouse.y) * 0.05
-
-      //   this.x = this.lastMouse.x + Math.cos(this.radians) * this.pathRadius;
-      //   this.y = this.lastMouse.y + Math.sin(this.radians) * this.pathRadius;
-      //   this.draw(lastPoint);
-      // }
-
       draw() {
         c.beginPath()
+        c.setLineDash([5,2]);
         c.strokeStyle = this.color
         c.lineWidth = this.radius
         c.moveTo(this.x1, this.y1)
@@ -75,38 +61,65 @@ const RootLayout = ({ children }) => {
 
     }
 
+    // Objects
+    class DrawRandomShape {
+      constructor() {
+        this.radius = intInRange(100, 150)
+        this.x1 = intInRange(0, canvas.width)
+        this.y1 = intInRange(0, canvas.height)
+        this.thickness = intInRange(2, 3)
+        this.color = 'rgba(255,255,255,0.1)'
+      }
+
+      draw() {
+        c.beginPath()
+        c.strokeStyle = this.color
+        c.lineWidth = this.thickness
+        c.arc(this.x1, this.y1, this.radius, 0, Math.PI * 2, false)
+        c.stroke()
+        c.closePath()
+      }
+
+    }
+
     // Implementation
     let linesX
     let linesY
+    let randomShape
     function init() {
       linesX = []
       linesY = []
+      randomShape = new DrawRandomShape()
       let gridsize = 5;
-      let xIncrement = canvas.width/gridsize
-      let yIncrement = canvas.height/gridsize
+      let xIncrement = canvas.width / gridsize
+      let yIncrement = canvas.height / gridsize
       console.log(xIncrement)
 
       let xpos = 0
       let ypos = 0
       for (let i = 0; i < gridsize; i++) {
         const thickness = Math.random() * 1 + 1;
-        linesX.push(new Line(xpos, 0, xpos, canvas.height, thickness, 'rgb(255,255,255, 0.25)'))
+        linesX.push(new Line(xpos, 0, xpos, canvas.height, thickness, 'rgb(255,255,255, 0.1)'))
         xpos += xIncrement
       }
 
       for (let i = 0; i < gridsize; i++) {
         const thickness = Math.random() * 1 + 1;
-        linesY.push(new Line(0, ypos, canvas.width, ypos, thickness, 'rgb(255,255,255, 0.25)'))
+        linesY.push(new Line(0, ypos, canvas.width, ypos, thickness, 'rgb(255,255,255, 0.1)'))
         ypos += yIncrement
       }
-    }
-    // Animation Loop
-    function animate() {
-      requestAnimationFrame(animate)
-      c.fillStyle = 'rgb(0,0,0)'
-      c.fillRect(0, 0, canvas.width, canvas.height)
-      // c.clearRect(0,0,canvas.width,canvas.height)
 
+      const grd = c.createLinearGradient(0, 0, canvas.width, canvas.height);
+      grd.addColorStop(0, "rgba(0, 0, 0, 1)");
+      grd.addColorStop(0.2, "rgba(0, 0, 0, 0.95)");
+      grd.addColorStop(0.4, "rgba(0, 0, 0, 0.90)");
+      grd.addColorStop(0.6, "rgba(0, 0, 0, 0.85)");
+      grd.addColorStop(0.8, "rgba(0, 0, 0, 0.80)");
+      grd.addColorStop(1, "rgba(0, 0, 0, 0.75)");
+      c.fillStyle = grd
+      c.fillRect(0, 0, canvas.width, canvas.height)
+
+      randomShape.draw()
       linesX.forEach(line => {
         line.draw()
       })
@@ -117,8 +130,6 @@ const RootLayout = ({ children }) => {
     }
 
     init()
-    animate()
-
   }, [])
 
   return (
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,64 @@
-import Footer from "@components/Footer"
+"use client"
 import CODESVG from "@components/svg"
 import "@styles/home.css"
+import Lottie from "lottie-react"
+import animationData from "../../public/assets/animation.json"
+import apiData from "../../public/assets/api.json"
+import officeData from "../../public/assets/office.json"
 
 const Home = () => {
     return (
         <div id="container">
             <div className="aboutme">
-                <h1 className="name">Jaykumar Patel | Full Stack developer 💻
+                <h1 className="name">Jaykumar Patel Full Stack developer 💻
                 </h1>
                 <div className="codesvg">
-                <CODESVG/>
+                    <Lottie animationData={animationData} />
                 </div>
                 <p className="desc">
-                My technical skill set spans a diverse range, encompassing the robust Java programming language, which I've employed to architect and develop resilient APIs serving as application cornerstones. Additionally, my proficiency in the SpringBoot Framework has enabled me to construct scalable and high-performance applications, streamlining development workflows. I'm adept at harnessing the power of VueJS to create dynamic and user-centric frontend interfaces, ensuring seamless and intuitive interactions.
+                    My technical skill set spans a diverse range, encompassing the robust Java programming language, which I've employed to architect and develop resilient APIs serving as application cornerstones. Additionally, my proficiency in the SpringBoot Framework has enabled me to construct scalable and high-performance applications, streamlining development workflows. I'm adept at harnessing the power of VueJS to create dynamic and user-centric frontend interfaces, ensuring seamless and intuitive interactions.
                 </p>
             </div>
-            <Footer />
+
+            <div className="techstack">
+                <h1 className="name">TechStack Experience 📝
+                </h1>
+                <div className="codesvg">
+                    <CODESVG />
+                </div>
+                <p className="desc">
+                    <em>#PrimaryStack🗝️</em>
+                    The React, Oracle SQL Server, and Spring Boot tech stack integrates a modern JavaScript library (React), a robust relational database system (Oracle SQL Server), and a Java-based backend framework (Spring Boot) to facilitate the creation of sophisticated web applications with interactive user interfaces, powerful data management, and efficient server-side functionality.
+                </p>
+                <p className="desc">
+                    The React, MongoDB, and Node.js tech stack which combines a JavaScript-based frontend library (React), a flexible NoSQL database (MongoDB), and an asynchronous backend runtime (Node.js) to enable the efficient development of dynamic, real-time web applications with a unified language and versatile data management capabilities.
+                </p>
+            </div>
+
+            <div className="experience">
+                <h1 className="name">Corporate Experience 💼
+                </h1>
+                <div className="codesvg">
+                    <Lottie animationData={apiData} />
+                </div>
+                <div className="codesvg">
+                    <Lottie animationData={officeData} />
+                </div>
+                <p className="desc">
+                    <span>Java and SpringBoot Framework</span>
+                    <span>API Design and Development</span>
+                    <span>Frontend Framework (VueJS)</span>
+                    <span>User Experience (UX) Design</span>
+                    <span>Cloud Infrastructure (Microsoft Azure)</span>
+                    <span>DevOps Collaboration</span>
+                    <span>Agile/Scrum Methodologies</span>
+                    <span>Cross-Functional Team Collaboration</span>
+                    <span>Legacy Application Replacement</span>
+                    <span>Client Relationship Management</span>
+                    <span>Innovation and Quality Delivery</span>
+                    <span>End-to-End Full Stack Development</span>
+                </p>
+            </div>
         </div>
     )
 }
